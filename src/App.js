4ec651091f01cd7236670f22f8d1593a726d1135
MarianoLibre <mariano.macri@mercadolibre.com>
2023-07-30 11:50:08 -0300

    Remove "tailwindcss" because it's SHIT

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
 export function App() {
-  return (
-    <div className="p-2 min-h-screen min-w-screen bg-slate-600 box-border block">
-      <h1 className="bg-slate-500 text-white font-mono p-4 rounded border border-slate-500 border-t-slate-400 shadow-md">No More Fucking Tabs!</h1>
-    </div>
-  );
+  return <h1>No More Fucking Tabs!</h1>
 }
